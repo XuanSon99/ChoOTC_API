7e57208a49b766276e33030f1c0632612bd53cbf
Your Name <you@example.com>
2025-03-15 21:32:07 +0700

    change

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -30,7 +30,7 @@ class HistoryController extends Controller
     public function hisUser(Request $request)
     {
         $uid = $request->route('uid');
-        $his = History::where("uid", $uid)->get()->toArray();
+        $his = History::where("uid", $uid)->where("status", 'p')->get()->toArray();
         $value = array_sum(array_column($his,'amount'));
         return $value;
     }
